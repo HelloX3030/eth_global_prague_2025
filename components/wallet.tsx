b2619a77d4e8d8f47e3a428035c2e6503389b340
Reptudn <jonas.kauker2003@gmail.com>
2025-05-31 14:12:59 +0200

    added supabase js with react native async storage and polyfill

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -1,26 +1,26 @@
-// import { ethers } from 'ethers'
+import { ethers } from 'ethers'
 
-// declare global {
-// 	interface Window {
-// 		ethereum?: any;
-// 	}
-// }
+declare global {
+	interface Window {
+		ethereum?: any;
+	}
+}
 
-// export async function connectWallet() {
-// 	try {
-// 		if (!window.ethereum) {
-// 			alert("No wallet found")
-// 			return { provider: null, signer: null, address: null }
-// 		}
+export async function connectWallet() {
+	try {
+		if (!window.ethereum) {
+			alert("No wallet found")
+			return { provider: null, signer: null, address: null }
+		}
 
-// 		const provider = new ethers.BrowserProvider(window.ethereum)
-// 		await provider.send("eth_requestAccounts", [])
-// 		const signer = await provider.getSigner()
-// 		const address = await signer.getAddress()
-// 		return { provider, signer, address }
-// 	} catch (error) {
-// 		console.error("Error connecting wallet:", error)
-// 		alert("Failed to connect wallet")
-// 		return { provider: null, signer: null, address: null }
-// 	}
-// }
+		const provider = new ethers.BrowserProvider(window.ethereum)
+		await provider.send("eth_requestAccounts", [])
+		const signer = await provider.getSigner()
+		const address = await signer.getAddress()
+		return { provider, signer, address }
+	} catch (error) {
+		console.error("Error connecting wallet:", error)
+		alert("Failed to connect wallet")
+		return { provider: null, signer: null, address: null }
+	}
+}
