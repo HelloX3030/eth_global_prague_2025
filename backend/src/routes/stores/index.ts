4dfc656b5753bca58d15defb8c11a04a1939a04e
Reptudn <jonas.kauker2003@gmail.com>
2025-05-31 22:33:22 +0200

    removed backend

--- backend/src/routes/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from 'fastify'
-
-const stores: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/all', async function (request, reply) {
-    return ("Stores API is working");
-  })
-}
-
-export default stores;
